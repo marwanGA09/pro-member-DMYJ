dc8f228fe43aec3d1a228ccaabe5206dc7621e54
Adem Kedir <ademkedir724@gmail.com>
2024-11-24 23:00:33 +0300

    structure folder of client

---
+++ client/src/assets/pages/Login.jsx
@@ -0,0 +1,16 @@
+function Login() {
+  return (
+    <div className="">
+      <h1>login now</h1>
+      <form action="">
+        <label htmlFor="username">User name: </label>
+        <input type="username" />
+        <label htmlFor="password">password: </label>
+        <input type="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
