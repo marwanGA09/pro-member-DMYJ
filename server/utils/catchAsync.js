54a1b869be0ac2fe9f9b7d7835e93974718b34ee
Adem Kedir <ademkedir724@gmail.com>
2024-12-16 15:04:18 +0300

    update catchAsync function

--- server/utils/catchAsync.js
+++ server/utils/catchAsync.js
@@ -1,5 +1,10 @@
+// const catchAsync = (fn) => {
+//   return (req, res, next) => fn(req, res).catch((err) => next(err));
+// };
+
+// module.exports = catchAsync;
 const catchAsync = (fn) => {
-  return (req, res, next) => fn(req, res).catch((err) => next(err));
+  return (req, res, next) => fn(req, res, next).catch((err) => next(err));
 };
 
 module.exports = catchAsync;
