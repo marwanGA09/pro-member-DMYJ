560ef160a498c9f4a312eb134922244c4347c5ab
Adem Kedir <ademkedir724@gmail.com>
2025-02-04 22:40:54 +0300

    change nav

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,19 +1,18 @@
 import { useContext, useState } from 'react';
-import { Link } from 'react-router-dom'; // Changed to use `react-router-dom` for Link
-import './Navigation.scss';
+import { Link } from 'react-router-dom';
+import styles from './Navigation.module.scss';
 import { globalContext } from '../components/ContextProvider';
 import axios from './../Utils/axios';
+import { Cloudinary } from '@cloudinary/url-gen';
+import { AdvancedImage } from '@cloudinary/react';
 
 const Navigation = () => {
   const { user, setUser } = useContext(globalContext);
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // State for toggling menu
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLogout = () => {
     axios
-      .get('logout', {
-        withCredentials: true,
-        // headers: { Authorization: `Bearer ${user.token}` },
-      })
+      .get('logout', { withCredentials: true })
       .then((res) => {
         console.log(res);
         setUser(false);
@@ -21,59 +20,75 @@ const Navigation = () => {
       .catch((e) => {
         console.log(e.response?.data);
       });
-    // alert('You have been logged out.');
   };
 
-  console.log('user', user);
-
+  console.log('user navigation', user);
+  console.log({ styles });
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen); // Toggle menu visibility
+    setIsMenuOpen(!isMenuOpen);
   };
+
+  const cld = new Cloudinary({
+    cloud: { cloudName: import.meta.env.VITE_CLOUDINARY_NAME },
+  });
+  console.log(':Kjsldf', user);
 
   return (
-    <nav className="navigation">
+    <nav className={styles.navigation}>
       <Link to={'/'}>
-        <div className="logo">MyLogo</div>
+        <div className={styles.logo}>MyLogo</div>
       </Link>
 
       {/* Hamburger Icon */}
-      <div className="hamburger" onClick={toggleMenu}>
+      <div className={styles.hamburger} onClick={toggleMenu}>
         ☰
       </div>
 
       {/* Menu Links */}
       <ul
-        className={`nav-links ${isMenuOpen ? 'open' : ''}`}
+        className={`${styles.navLinks} ${isMenuOpen ? styles.open : ''}`}
         onClick={toggleMenu}
       >
-        <li>
+        <li className={styles.navItem}>
           <Link to={'/'}>Home</Link>
         </li>
-        <li>
+        <li className={styles.navItem}>
           <Link to={'/about'}>About</Link>
         </li>
         {user ? (
           <>
             {user.user.role === 'admin' && (
-              <li>
+              <li className={styles.navItem}>
                 <Link to={'/admin-dashboard'}>Admin</Link>
               </li>
             )}
-            <li>
+            <li className={styles.navItem}>
               <Link to={'/Dashboard/members'}>Dashboard</Link>
             </li>
-            <li>
-              <button className="logout-button" onClick={handleLogout}>
+            <li className={styles.navItem}>
+              <div className={styles.currentUser}>
+                <AdvancedImage
+                  cldImg={cld.image(user.user.profileUrl)}
+                  className={styles.profileImage}
+                />
+                <div className={styles.userInfo}>
+                  <span className={styles.username}>{user.user.username}</span>
+                  <span className={styles.role}>{user.user.role}</span>
+                </div>
+              </div>
+            </li>
+            <li className={styles.navItem}>
+              <button className={styles.logoutButton} onClick={handleLogout}>
                 Logout
               </button>
             </li>
           </>
         ) : (
           <>
-            <li>
+            <li className={styles.navItem}>
               <Link to={'/login'}>Login</Link>
             </li>
-            <li>
+            <li className={styles.navItem}>
               <Link to={'/signup'}>SignUp</Link>
             </li>
           </>
