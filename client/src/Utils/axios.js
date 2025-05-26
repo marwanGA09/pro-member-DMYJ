946a097b5344a7dd34246c173464b639c6d69050
Adem Kedir <ademkedir724@gmail.com>
2025-05-26 14:58:56 +0300

    add readme file

--- client/src/Utils/axios.js
+++ client/src/Utils/axios.js
@@ -4,10 +4,10 @@ const axios = ax.create({
   // baseURL: 'http://localhost:4321/v1/',
 
   // NOTE :baseURL on render
-  baseURL: 'https://pro-member-dmyj.onrender.com/v1/',
+  // baseURL: 'https://pro-member-dmyj.onrender.com/v1/',
 
   // NOTE :baseURL on MY DOMAIN
-  // baseURL: 'https://apipro.deramyj.com/v1/',
+  baseURL: 'https://apipro.deramyj.com/v1/',
 });
 
 export default axios;
